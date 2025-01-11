a580c0bed1df6abdb735148879e55a168180563e
Esakkipandi0214 <praveenpandian01@gmail.com>
2025-01-11 17:28:37 +0530

    updates

--- src/Components/Home/ContentHome.tsx
+++ src/Components/Home/ContentHome.tsx
@@ -4,7 +4,7 @@ const Explore = () => {
   return (
     <div className="text-center space-y-72 items-stretch pt-32 tab_01:pt-[12%]  h-[90%] w-full">
         <div className=" flex flex-col gap-4">
-      <h1 className=" tab_01:text-7xl text-4xl font-bold text-[#5D8736] gap-5 flex justify-center items-center">PAPPU <p className=" text-[#FFFDF0]">TRAVELS</p></h1>
+      <h1 className=" tab_01:text-7xl text-4xl font-bold text-[#F6F5F5] gap-5 flex justify-center items-center">PAPPU <p className=" text-[#FFF3CF]">TRAVELS</p></h1>
       <p className=" tab_01:text-6xl text-lg text-white">Discover new places and adventures</p>
       </div>
       <FavouriteLocation/>
