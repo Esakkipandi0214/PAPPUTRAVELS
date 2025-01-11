a937df5c09f72530aa3ab09b45cb788946ab9d25
Esakkipandi0214 <praveenpandian01@gmail.com>
2025-01-11 23:24:02 +0530

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        tab_01: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
 }
